068f4d13bb6d7260d8987209ecfda9d80f8b74f3
Daniel Newton <kdaniel.newton@gmail.com>
2024-03-16 03:01:57 -0500

    Add sync solution

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,29 @@
 "use strict";
 
+const Heap = require('heap');
+
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  const heap = new Heap((a, b) => a.date - b.date);
+  for (let i = 0; i < logSources.length; i++) {
+    const log = logSources[i].pop();
+    if (log) {
+      heap.push({ sourceIndex: i, ...log});
+    }
+  }
+
+  while (heap.size() > 0) {
+    const { sourceIndex, ...log } = heap.pop();
+    printer.print(log);
+
+    const nextLog = logSources[sourceIndex].pop();
+    if (nextLog) {
+      heap.push({ sourceIndex, ...nextLog });
+    }
+  }
+
+  printer.done();
+
   return console.log("Sync sort complete.");
 };
